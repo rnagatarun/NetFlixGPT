eaf7955944a27d691b20e80091f6a24f93de6650
NagaTarun <rnagatarun@gmail.com>
2025-05-10 00:36:12 +0530

    call all the categories and update them on store and secondary container for movie list categories

--- src/components/SecondaryContainer.tsx
+++ src/components/SecondaryContainer.tsx
@@ -1,10 +1,32 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../utils/appStore";
+import MovieList from "./MovieList";
 
 const SecondaryContainer = () => {
+  const movies = useSelector((store: RootState) => store.movies);
   return (
-    <div>
-      
+    <div className="bg-black">
+      <div className="-mt-70 relative z-20 pl-8">
+        <MovieList
+          title={"Now Playing"}
+          movies={movies.nowPlayingMovies || []}
+        />
+        <MovieList
+          title={"Top Rated"}
+          movies={movies.topRatedMovies || []}
+        />
+         <MovieList
+          title={"Popular"}
+          movies={movies.popularMovies || []}
+        />
+        <MovieList
+          title={"Upcoming"}
+          movies={movies.upcomingMovies || []}
+        />
+        
+      </div> 
     </div>
-  )
-}
+  );
+};
 
-export default SecondaryContainer
+export default SecondaryContainer;
