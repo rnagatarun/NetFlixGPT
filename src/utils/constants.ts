b425f34c2cfdb4649d03ce45b0bde439af1fedaa
NagaTarun <rnagatarun@gmail.com>
2025-05-10 17:47:49 +0530

    add supported languages in constants and actual translations on languageConstants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,10 +2,15 @@ export const NETFLIX_LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTru
 export const SITE_UI_BACKGROUND = "https://assets.nflxext.com/ffe/siteui/vlv3/fa7be975-efc3-48c6-8188-f07fdd1aa476/web/IN-en-20250428-TRIFECTA-perspective_e045264e-b4d4-4a6f-b2cc-f95e3344a332_large.jpg";
 export const USER_ICON = "https://occ-0-6246-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDQ3MmExODE3YzM2NmE5Nzk2ZjJlOWE5ZTY4ZTNmNiIsIm5iZiI6MS43NDY3MTUwNTc3NDg5OTk4ZSs5LCJzdWIiOiI2ODFjYzFiMTRlOTM5NzZhODQwMWVhMTMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.P1YEm-aSTkZgYeI3RlFjK7r7VCuwXRpXeO8syUpOdbQ'
-    }
-  };
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDQ3MmExODE3YzM2NmE5Nzk2ZjJlOWE5ZTY4ZTNmNiIsIm5iZiI6MS43NDY3MTUwNTc3NDg5OTk4ZSs5LCJzdWIiOiI2ODFjYzFiMTRlOTM5NzZhODQwMWVhMTMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.P1YEm-aSTkZgYeI3RlFjK7r7VCuwXRpXeO8syUpOdbQ'
+  }
+};
 export const IMG_CDN = "https://image.tmdb.org/t/p/w780";
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "french", name: "French" },
+]
---
+++ src/utils/languageConstants.ts
@@ -0,0 +1,18 @@
+const language = {
+    en: { 
+        search: "Search",
+        gptSearchPlaceholder: "What would you like to watch today?"
+     },
+
+     hindi: {
+        search: "खोज",
+        gptSearchPlaceholder: "आज आप क्या देखना चाहेंगे?"
+     },
+
+     french: {
+        search:"Recherche",
+        gptSearchPlaceholder: "Qu'aimeriez-vous regarder aujourd'hui ?"
+     }
+}
+
+export default language;
